d3a295de7ce6d4d63f9017835a6d0db2ce9a8d5e
igorhgoncalvess <igorhgoncalvess@gmail.com>
2023-12-03 01:39:33 -0300

    Suicidio é uma boa opção. 01:39AM 03/12/2023

--- Loja/app/secundary.js
+++ Loja/app/secundary.js
@@ -1,41 +1,79 @@
 const eyes = document.querySelectorAll('.eye')
-const login = document.getElementById('login')
-const register = document.getElementById('register')
+const loginBtn = document.getElementById('login')
+const registerBtn = document.getElementById('register')
 const logo = document.getElementById('logo')
+const loginArrow = document.querySelector('#login-arrow')
 
 // Adicionando interação aos corações
-function heart() {
+function heart(logged) {
     const hearts = document.querySelectorAll('.heart')
     hearts.forEach(heart => {
         heart.onclick = () => {
-            heart.classList.toggle('liked')
-            if (heart.classList.contains('liked')) {
-                heart.src = '../assets/heartfill.png'
-                console.log(heart.parentNode.parentNode)
-            } else {
-                heart.src = '../assets/heart.png'
+            if(logged){
+                heart.classList.toggle('liked')
+                if (heart.classList.contains('liked')) {
+                    heart.src = '../assets/heartfill.png'
+                } else {
+                    heart.src = '../assets/heart.png'
+                }
+            }else{
+                heart.parentNode.querySelector('.heart-text').classList.remove('hidden')
+                setTimeout(() => {
+                    heart.parentNode.querySelector('.heart-text').classList.add('hidden')
+                }, 1500)
             }
         }
     })
 }
 
+// Função para mostrar as senhas na tela de login
+
+loginArrow.onclick = () => {
+    document.querySelector('.logins-view').classList.toggle('active')
+}
+
 // Funções para abrir as telas de Login e Register
-logo.onclick = () => {
+
+logo.onclick = function(){
+    if(document.querySelector('#username').innerHTML != ''){
+        menu(true)
+    }else{
+        menu(false)
+    }
+    
+}
+
+function menu(logged, username){
+    if(logged){
+        if(username != null){
+            user = username
+        }
+        document.querySelector('.buttons').classList.add('hidden')
+        document.querySelector('.user').classList.remove('hidden')
+        
+        document.querySelector('#username').innerHTML = `Bem vindo(a), ${user}`
+    }else{
+        document.querySelector('.buttons').classList.remove('hidden')
+        document.querySelector('.user').classList.add('hidden')
+        document.querySelector('#username').innerHTML = ''
+    }
     document.querySelector('.container').classList.remove('hidden')
     document.querySelector('.login').classList.add('hidden')
     document.querySelector('.register').classList.add('hidden')
 }
-login.onclick = () => {
+loginBtn.onclick = () => {
     document.querySelector('body').scrollTo(0, 0)
     document.querySelector('.container').classList.add('hidden')
     document.querySelector('.login').classList.remove('hidden')
     document.querySelector('.register').classList.add('hidden')
+    document.querySelector('.login-animation').classList.remove('hidden')
 }
 
-register.onclick = () => {
+registerBtn.onclick = () => {
     document.querySelector('.container').classList.add('hidden')
     document.querySelector('.login').classList.add('hidden')
     document.querySelector('.register').classList.remove('hidden')
+    document.querySelector('.login-animation').classList.remove('hidden')
 }
 
 // Mostrar e esconder senha
